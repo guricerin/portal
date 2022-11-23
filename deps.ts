9780de923f01ccd08c1384dc9f894a2adec7b84e
guricerin <chanbo1e9@gmail.com>
2022-11-24 00:09:25 +0900

    use vanta

--- deps.ts
+++ deps.ts
@@ -1,6 +1,5 @@
 export { default as React } from "https://cdn.skypack.dev/react@17.0.2?dts";
 export { default as ReactDOM } from "https://cdn.skypack.dev/react-dom@17.0.2?dts";
-export { default as ReactIcon } from "https://cdn.skypack.dev/react-icons@4.6.0?dts";
 export {
   FaBlog,
   FaCode,
