4836b4706f586d92795822c87696cd6a380dfd51
guricerin <chanbo1e9@gmail.com>
2024-02-23 23:47:12 +0900

    css

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -7,9 +7,11 @@ interface AvatorProps {
 
 export const Home: FC<AvatorProps> = (props: AvatorProps) => {
   return (
-    <div class="avator">
-      <img src={props.iconPath} />
-      <span>{props.name}</span>
+    <div class="container my-5">
+      <div class="avator">
+        <img src={props.iconPath} />
+        <span>{props.name}</span>
+      </div>
     </div>
   )
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -13,11 +13,13 @@ export const Layout: FC<SiteDataProps> = (props: SiteDataProps) => html`<!doctyp
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta property="og:url" content="https://nitro6th.net" />
       <meta property="og:title" content="${props.title}" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@materializecss/materialize@2.0.3/dist/css/materialize.min.css">
       <link rel="stylesheet" href="css/style.css" />
       <link rel="icon" href="favicon.ico" />
       <title>${props.title}</title>
     </head>
     <body>
-      <main class='container'>${props.children}</main>
+      <main class='contents'>${props.children}</main>
+      <script src="https://cdn.jsdelivr.net/npm/@materializecss/materialize@2.0.3/dist/js/materialize.min.js"></script>
     </body>
   </html>`
