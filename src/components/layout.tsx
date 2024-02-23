af84616e9f76252d15c1a13e631686de97918a66
guricerin <chanbo1e9@gmail.com>
2024-02-23 17:39:57 +0900

    fix css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
-import type { FC } from "hono/jsx"
+import type { FC } from 'hono/jsx'
 import { globalCss } from '../styles/global-css'
+import { Style } from 'hono/css'
 
 export const Layout: FC = () => {
   return (
@@ -8,6 +9,8 @@ export const Layout: FC = () => {
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:url" content="https://guricerin.github.io/portal/" />
+        <title>guricerin's portal</title>
+        <Style />
       </head>
       <body>
         <main>
--- src/styles/global-css.tsx
+++ src/styles/global-css.tsx
@@ -10,6 +10,5 @@ body {
   padding: 1em 2em;
   background: linear-gradient(83.21deg,#3245ff 0%,#bc52ee 100%);
   color: white;
-  height: 100vh;
 }
 `
